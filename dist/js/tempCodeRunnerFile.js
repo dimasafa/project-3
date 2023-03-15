2f15425bbcf1cc2cfd5bb5c4fde747dff7a2bdd9
Dmitrii <dimasafanasyev@gmail.com>
2023-03-15 15:13:00 +0100

    fertig

--- dist/js/tempCodeRunnerFile.js
+++ dist/js/tempCodeRunnerFile.js
@@ -1,6 +1,31 @@
-    const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
-    
-    console.log(`${hours}:${minutes}:${seconds}`);
+    const {MongoClient} = require('mongodb');
+    const client = new MongoClient('mongodb+srv://dimas-aa:Himdimas123@cluster0.muk3gwu.mongodb.net/mongo?retryWrites=true&w=majority');
+    const start = async () => {
+        try {
+            
+            await client.connect();
+            console.log('CONNECT');
+    /*         await client.db().createCollection('users_WD'); */
+            
+
+            const users = client.db().collection('users_WD');
+
+            const form = document.querySelector('form');
+            const formData = new FormData(form);
+            const data = {};
+            
+            for (const [key, value] of formData.entries()) {
+            data[key] = value;
+            }
+            
+
+            await users.insertOne(data);
+
+    /*         const user = await users.findOne({age: 34}); */
+            console.log(users);
+        } catch (e) {
+            console.log(e);
+        } 
+    }
+
+    start();
--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,54 @@
-/* const {MongoClient} = require('mongodb');
+/* const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const client = new MongoClient('mongodb+srv://dimas-aa:Himdimas123@cluster0.muk3gwu.mongodb.net/mongo?retryWrites=true&w=majority');
+// parse incoming request bodies in a middleware before your handlers
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const start = async () => {
-    try {
-        await client.connect();
-        console.log('CONNECT');
-        await client.db().createCollection('users+');
+// connect to your MongoDB Atlas cluster
+mongoose.connect("mongodb+srv://dimas-aa:Himdimas123@cluster0.muk3gwu.mongodb.net/mongo", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-        const users = client.db().collection('users+');
-        await users.insertOne({name: 'Dmitrii', age: 34});
+// create a schema for your data
+const notesSchema = {
+  name: String,
+  email: String,
+  company: String
+};
 
-        const user = await users.findOne({age: 34});
-        console.log(user);
-    } catch (e) {
-        console.log(e);
-    } 
-}
+// create a model for your data
+const Note = mongoose.model("Note", notesSchema);
 
-start(); */
+// serve your index.html file
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// handle form submission
+app.post("/submit", function(req, res) {
+  // create a new Note object with the form data
+  const newNote = new Note({
+    name: req.body.name,
+    email: req.body.email,
+    company: req.body.company
+  });
+
+  // save the new note to your database
+  newNote.save(function(err) {
+    if (err) {
+      console.log(err);
+      res.redirect("/error.html");
+    } else {
+      console.log("Successfully saved note to database.");
+      res.redirect("/");
+    }
+  });
+});
+
+// start your server
+app.listen(3000, function() {
+  console.log("Server is running on port 3000.");
+}); */
